cb6c70f36f31c65a36ee1e7fad8c1835d015bc06
Stephen C. Sanders <scsandman6496@gmail.com>
2016-11-05 18:51:51 -0400

    updated source hierarchy, additional minor edits

--- src/scripts/main1.js
+++ src/scripts/main1.js
@@ -5,8 +5,17 @@ $(document).ready(function() {
 
 // Convert to .txt file
 function txtDoc1() {
+  var form = document.getElementById("text-options");
   var text = document.getElementById("text").value;
   var filename = document.getElementById("text-filename").value;
   var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+
+  // Validate that file extension is .txt and nothing else
+  var re = /(\.txt)$/i;
+  if (!re.exec(filename)) {
+    alert("This application only supports .txt file downloads.");
+    break;
+  }
+
   saveAs(blob, filename);
 }
